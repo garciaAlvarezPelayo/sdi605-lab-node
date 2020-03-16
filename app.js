96b97cf58647ddbde42dbd7e7502c51a2bb8eae2
garciaAlvarezPelayo <xxerpelaxx@gmail.com>
2020-03-16 09:51:12 +0100

    SDI-NodeJS-.3,4,5_Express,_depuración_y_variables_de_entorno

---
+++ app.js
@@ -0,0 +1,20 @@
+// Módulos
+let express = require('express');
+let app = express();
+
+// Variables
+app.set('port', 8081);
+
+app.get('/usuarios', function(req, res){
+    console.log("Depurar aqui");
+    res.send('ver usuarios');
+});
+
+app.get('/canciones', function (req, res) {
+    res.send('ver canciones');
+});
+
+//lanzar servidor
+app.listen(app.get('port'), function () {
+    console.log("Servidor activo");
+});
